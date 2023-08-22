67f4e58402d71281ba9a6c4b786fe7037fd177d4
gbouras13 <george.bouras@adelaide.edu.au>
2023-08-22 14:05:36 +0930

    main

--- tcga_cptac_taxonomic_profiler/tcga_cptac_taxonomic_profiler/__main__.py
+++ tcga_cptac_taxonomic_profiler/tcga_cptac_taxonomic_profiler/__main__.py
@@ -313,6 +313,54 @@ def mmseqs(_input, output, log, **kwargs):
     )
 
 
+help_msg_extra_assembly = """
+\b
+CLUSTER EXECUTION:
+tcga_cptac_taxonomic_profiler assembly ... --profile [profile]
+For information on Snakemake profiles see:
+https://snakemake.readthedocs.io/en/stable/executing/cli.html#profiles
+\b
+RUN EXAMPLES:
+Required:           tcga_cptac_taxonomic_profiler assembly --input [file]
+Specify threads:    tcga_cptac_taxonomic_profiler assembly ... --threads [threads]
+Disable conda:      tcga_cptac_taxonomic_profiler assembly ... --no-use-conda 
+Change defaults:    tcga_cptac_taxonomic_profiler assembly ... --snake-default="-k --nolock"
+Add Snakemake args: tcga_cptac_taxonomic_profiler assembly ... --dry-run --keep-going --touch
+Specify targets:    tcga_cptac_taxonomic_profiler assembly ... all print_targets
+Available targets:
+    all             Run everything (default)
+    print_targets   List available targets
+"""
+
+
+@click.command(
+    epilog=help_msg_extra_mmseqs,
+    context_settings=dict(
+        help_option_names=["-h", "--help"], ignore_unknown_options=True
+    ),
+)
+@click.option("--input", "_input", help="Input directory", type=str, required=True)
+@common_options
+def mmseqs(_input, output, log, **kwargs):
+    """Run TCGA_CPTAC_Taxonomic_Profiler to assemble MAGs """
+    # Config to add or update in configfile
+    merge_config = {
+        "input": _input,
+        "output": output,
+        "log": log
+    }
+
+    # run!
+    run_snakemake(
+        # Full path to Snakefile
+        snakefile_path=snake_base(os.path.join("workflow", "run_assembly.smk")),
+        system_config=snake_base(os.path.join("config", "config.yaml")),
+        merge_config=merge_config,
+        log=log,
+        **kwargs
+    )
+
+
 @click.command()
 @common_options
 def config(configfile, **kwargs):
